4c1617dfaaf77b6e7955e77e4559b079351f407c
haykmaryanyan <haykmaryanyan@mail.ru>
2015-06-03 11:53:53 +0400

    hello and php files 03.06.2015 11:53 commit

--- css/hello.php
+++ css/hello.php
@@ -2,5 +2,5 @@
 echo "Hello World and hello Hayk"
 ?>
 <p> The changes made for diff aaaaa bbbbbbbbbbb</p>
-<p> ccccccccccccc</p>
-<p> kdljfdlkfjldkjf</p>
+<p> cccc dddddddddddddddddddd ddddddd</p>
+<p> kdljfdlkfjldkjf branching changes</p>
